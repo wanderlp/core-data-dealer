71803b4b6b7dd2060a9b0aaf869950e2c47a9c64
Wanderson <lopez.wanderson@gmail.com>
2022-04-22 16:12:53 -0600

    Initial Commit

---
+++ CoreDataDealer/CoreDataDealerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDealerApp.swift
+//  CoreDataDealer
+//
+//  Created by Wanderson López on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDealerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
